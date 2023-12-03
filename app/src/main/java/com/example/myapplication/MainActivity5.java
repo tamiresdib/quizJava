6a2c9373c2a4f54e9dde45b46ce73755045763da
tamires <tamiresdib6@gmail.com>
2023-12-03 20:58:35 -0300

    telas quiz

--- app/src/main/java/com/example/myapplication/MainActivity5.java
+++ app/src/main/java/com/example/myapplication/MainActivity5.java
@@ -5,18 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity5 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main5);
     }
 
-    public void abrirTelaNome(View view) {
-        Intent intent = new Intent(this, MainActivity5.class);
+    public void abrirTelaPrincipal(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
-
 }
